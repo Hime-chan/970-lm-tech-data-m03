add214e447693fb7f1ef1503c58e720e929c5d7d
Claudia Cavalcante Fonseca <himehimur@gmail.com>
2023-03-29 15:24:57 -0300

    feat: Adicionada a função de soma à calculadora

--- projetos/calculadora/__init__.py
+++ projetos/calculadora/__init__.py
@@ -1,4 +1,14 @@
 from funcoes import *;
+import sys;
+
+funcoes_menu={'adicao':adicao, '+':adicao};
 
 def calcule():
-    return True;
+  while (True):
+    try:
+      funcoes_menu[input('Digite sua operação:')](input('Digite um número a:'),input('Digite um número b:'));
+    except TypeError:
+      print("Ocorreu um erro:");
+      print(sys.exc_info()[1]);
+    except KeyError:  
+      print(f"Opção inválida. \n Nossas opções são: {','.join(list(funcoes_menu.keys()))}");
