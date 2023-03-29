1a1bcb50be26a672016e76b465b8f328d5a1775e
Claudia Cavalcante Fonseca <himehimur@gmail.com>
2023-03-29 16:02:34 -0300

    feat:Divisão adicionada à calculadora
    O vetor de funções foi atualizado incluindo "divisao" e "/".
    A função "divisao" foi criada usando a função "operacoes" e a função "divisao_aux", que levanta o erro de divisão por zero.

--- projetos/calculadora/__init__.py
+++ projetos/calculadora/__init__.py
@@ -1,12 +1,12 @@
 from funcoes import *;
 import sys;
 
-funcoes_menu={'adicao':adicao, '+':adicao,'subtracao':subtracao,'-':subtracao};
+funcoes_menu={'adicao':adicao, '+':adicao,'subtracao':subtracao,'-':subtracao, 'divisao':divisao,'/':divisao};
 
 def calcule():
   while (True):
     try:
-      funcoes_menu[input('Digite sua operação:')](input('Digite um número a:'),input('Digite um número b:'));
+      print(funcoes_menu[input('Digite sua operação:')](input('Digite um número a:'),input('Digite um número b:')));
     except TypeError:
       print("Ocorreu um erro:");
       print(sys.exc_info()[1]);
--- projetos/calculadora/funcoes.py
+++ projetos/calculadora/funcoes.py
@@ -6,13 +6,23 @@ def operacoes(a,b,operacao,char):
     erro=f"O input 'a' e 'b' devem ser números. Recebemos a={a}, {type(a)} e b={b}, {type(b)}";
     raise TypeError(erro);
   else:
-    print(a,char,b,'=',operacao(new_a,new_b));
+    return (a+char+b+'='+str(operacao(new_a,new_b)));
   
-
 def adicao(a,b):
-  operacoes(a,b,lambda x,y: x+y,"+");
+  return operacoes(a,b,lambda x,y: x+y,"+");
     
-
 def subtracao(a,b):
-  operacoes(a,b,lambda x,y: x-y,'-');
+  return operacoes(a,b,lambda x,y: x-y,'-');
+  
+def divisao_aux(a,b):
+  if (b==0):
+    raise TypeError("Divisão inválida! O denominador (dividendo) não pode ser zero.");
+    return 0;
+  else:
+    return a/b;
+  
+def divisao(a,b):
+  return operacoes(a,b,divisao_aux,'/');
+       
+  
   
