ae6e7678fa366117b9556cfe29907b6bc8ae8f9d
Claudia Cavalcante Fonseca <himehimur@gmail.com>
2023-03-29 21:05:35 -0300

    feat: Adição e utilização da função sair

--- projetos/calculadora/__init__.py
+++ projetos/calculadora/__init__.py
@@ -1,14 +1,17 @@
 from funcoes import *;
 import sys;
 
-funcoes_menu={'adicao':adicao, '+':adicao,'subtracao':subtracao,'-':subtracao, 'divisao':divisao,'/':divisao,'multiplicacao':multiplicacao,'*':multiplicacao};
+funcoes_menu={'adicao':adicao, '+':adicao,'subtracao':subtracao,'-':subtracao, 'divisao':divisao,'/':divisao,'multiplicacao':multiplicacao,'*':multiplicacao,'sair':sair};
 
 def calcule():
-  while (True):
+  x='';
+  while (x!='sair'):
     try:
-      print(funcoes_menu[input('Digite sua operação:')](input('Digite um número a:'),input('Digite um número b:')));
+      print(funcoes_menu[x:=input('Digite sua operação:')]((x=='sair' or input('Digite um número a:')),(x=='sair' or input('Digite um número b:'))));
     except TypeError:
       print("Ocorreu um erro:");
       print(sys.exc_info()[1]);
     except KeyError:  
       print(f"Opção inválida. \n Nossas opções são: {','.join(list(funcoes_menu.keys()))}");
+      
+      
